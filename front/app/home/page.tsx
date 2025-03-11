bc24a8b56048944ba8779c56334307dc5bd7d40a
arthursmarinho <arthursnat@gmail.com>
2025-03-11 00:42:47 -0300

    feat: add Home module with controller and service, update package dependencies

---
+++ front/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <>
+      <span>Oi Arthur </span>
+    </>
+  );
+}
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,6 +1,21 @@
-import Image from "next/image";
+// app/page.tsx
+"use client"; // necessário apenas se você for usar interatividade com hooks — aqui não é obrigatório
 
-export default async function Home() {
-  const res = await fetch("http://localhost:3000/mensagem");
-  return res.text();
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+        <p className="mt-4 text-lg">
+          Get started by editing <code>app/page.tsx</code>
+        </p>
+
+        <Link href="/home" className="text-blue-500 underline mt-8 block">
+          Go to Home
+        </Link>
+      </div>
+    </main>
+  );
 }
